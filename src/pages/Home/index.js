f52ecc8bf94c4d37ebaaf20bfd1cbfa1060b64a9
Juan <jb@hotmail.com>
2021-07-19 16:14:56 -0300

    pruebo un menú

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default () => <h1>Home</h1>;
+export default () => <>
+<h1>Bienvenidos a mi App de MARVEL</h1>
+<ul>
+        <li><Link to="/">Inicio</Link></li>
+        <li><Link to="/HeroList">Hero List</Link></li>
+        <li><Link to="/HeroDetails/">Hero Details</Link></li>
+        <li><Link to="/asd">Error en la url</Link></li>
+    </ul>
+</>;
