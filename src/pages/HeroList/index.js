7fd13997b82cf3bfc8b81f7bfc7ca991f9d46126
Juan <jb@hotmail.com>
2021-07-19 18:17:20 -0300

    doy forma al componente

--- src/pages/HeroList/index.js
+++ src/pages/HeroList/index.js
@@ -1,3 +1,24 @@
 import React from 'react';
+import Data from '../../data/marvel/characters.json';
 
-export default () => <h1>Hero List</h1>;
+export default function HeroList() {
+  return (
+    <div className="row row-cols-1 row-cols-md-3 g-4">
+      {Data.data.results.map((elem) => (
+        <div className="col">
+          <div className="card" style={{ width: '18rem' }}>
+            <img
+              src={`${elem.thumbnail.path}.${elem.thumbnail.extension}`}
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <p className="card-text, text-center">{elem.name}</p>
+            </div>
+          </div>
+          <div>{console.log(elem)}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
