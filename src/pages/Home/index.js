4b739bef1d8af11bc08aed8ee0ba80a95411ff50
Juan <jb@hotmail.com>
2021-07-19 18:28:28 -0300

    uso el componente menu creado anteriormente

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,22 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Menu from '../../application/components/Menu';
 
 export default function Home() {
-  <>
-    <h1>Bienvenidos a mi App de MARVEL</h1>
-    <ul>
-      <li>
-        <Link to="/">Inicio</Link>
-      </li>
-      <li>
-        <Link to="/HeroList">Hero List</Link>
-      </li>
-      <li>
-        <Link to="/HeroDetails/">Hero Details</Link>
-      </li>
-      <li>
-        <Link to="/asd">Error en la url</Link>
-      </li>
-    </ul>
-  </>;
+  return (
+    <>
+      <h1>Bienvenidos a mi App de MARVEL</h1>
+      <Menu />
+    </>
+  );
 }
